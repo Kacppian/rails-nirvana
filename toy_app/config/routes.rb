5203c7d78b3fef28164d49dad7328d50b8a9208b
Kacppian <aspkaushik@gmail.com>
2018-04-07 17:10:36 +0530

    Finished Static pages

--- toy_app/config/routes.rb
+++ toy_app/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'foo/bar'
+
+  get 'foo/baz'
+
   resources :microposts
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
